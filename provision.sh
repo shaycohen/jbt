26c3975654a622cc096703924d1ee331026d4497
root <root@odoo>
2019-12-10 09:37:06 -0800

    My first odoo! with database

--- provision.sh
+++ provision.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+apt-get update -y ; apt-get upgrade -y
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get install docker-ce docker-ce-cli containerd.io  -y
 
-sudo apt-get -y install apt-transport-https ca-certificates software-properties-common curl
-wget https://download.docker.com/linux/debian/gpg 
-sudo apt-key add gpg
-unlink gpg
-sudo add-apt-repository "deb https://download.docker.com/linux/debian stretch stable"
-sudo apt-get -y update
-sudo apt-get -y install docker-ce
-sudo usermod -aG docker vagrant
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-sudo systemctl start docker
-sudo systemctl enable docker
+docker-compose --version
 
-sudo docker version
+#get in oddo:
+#127.0.0.1:8069 in browser
 
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
+#databasename : odoo
+#password: odoo
+## docker-compose exec -u root odoo bash
+## cd /usr/lib/python3/dist-packages/odoo
 
-# docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo -e POSTGRES_DB=postgres --name db postgres:10
-# docker run -p 8069:8069 --name odoo --link db:db -t odoo
-
+#docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo -e POSTGRES_DB=postgres --name db postgres:10
+#docker run -p 8069:8069 --name odoo --link db:db -t odoo
